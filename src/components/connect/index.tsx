39093949fad676ea0f2c117ed6206bdb87304ada
user <keevahhphp@gmail.com>
2025-02-14 15:24:26 +0000

    connect 4

--- src/components/connect/index.tsx
+++ src/components/connect/index.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { useGameContext } from "../../lib/context.tsx/game-context";
 import React from "react";
-import DynamicButton from "../global/common/button";
 import PageHeader from "../global/common/page-header";
 import ConnectBoard from "./connect-board";
 
-export default function Connect4GameComponent() {
-  const { handleReset, gameOver } = useGameContext(); 
+export default function Connect4GameComponent() { 
 
   return (
     <div>
