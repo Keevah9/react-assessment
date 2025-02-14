3a91dc9b7e0acfd9710680f31807ba6365c349bd
user <user@Keevah.local>
2025-02-14 12:49:11 +0000

    connect 4

--- src/components/home/Connect4.tsx
+++ src/components/home/Connect4.tsx
@@ -6,7 +6,7 @@ export default function Connect4() {
   return (
     <section className='content-container'>
         <PageHeader title='Connect4 game' content='Play Connect4 game' /> 
-         <GameCard title='Connect4' url='/Connect4' img='/img/connect4.jpg' content='Created with react.js/next.js, typescript and Tailwind CSS' />
+         <GameCard title='Connect4' url='/connect4' img='/img/connect4.jpg' content='Created with react.js/next.js, typescript and Tailwind CSS' />
     </section>
   )
 }
