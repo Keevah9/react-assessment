29ebe136e0514de2b4e3ee723dbfb595a46b6153
user <keevahhphp@gmail.com>
2025-02-14 15:24:10 +0000

    connect 4

--- src/components/connect/index.tsx
+++ src/components/connect/index.tsx
@@ -7,19 +7,12 @@ import PageHeader from "../global/common/page-header";
 import ConnectBoard from "./connect-board";
 
 export default function Connect4GameComponent() {
-  const { handleReset } = useGameContext(); 
+  const { handleReset, gameOver } = useGameContext(); 
 
   return (
     <div>
       <PageHeader title="Connect 4" />
       <ConnectBoard />
-      <div className="flex flex-col items-center
-      justify-center pt-6">
-        <DynamicButton
-          onClick={handleReset} 
-          label="Restart"
-        />
-      </div>
     </div>
   );
 }
