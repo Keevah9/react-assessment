e0a48231d4bc707d83bff914bf874101c0785684
user <keevahhphp@gmail.com>
2025-02-14 15:39:16 +0000

    connect 4

--- src/components/connect/slot.tsx
+++ src/components/connect/slot.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { useGameContext } from "../../lib/context.tsx/game-context";
 
 interface SlotProps {
@@ -12,15 +13,18 @@ export const Slot: React.FC<SlotProps> = ({ ch, x, y }) => {
 
   return (
     <div
-      className="w-16 h-16 bg-white rounded-full flex cursor-pointer"
+      className="w-16 h-16 bg-white rounded-full flex cursor-pointer relative"
       data-col={x}
       onClick={() => !gameOver && handleMove(x)}
     >
       {ch && (
-        <img
+        <motion.img
           src={ch === "X" ? "/img/redtoken.svg" : "/img/blacktoken.svg"}
           alt={ch}
-          className="w-full h-full"
+          className="w-full h-full absolute"
+          initial={{ y: -200, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeOut", delay: y * 0.1 }}
         />
       )}
     </div>
