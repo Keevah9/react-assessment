eff4717366e8a00e5b44b11b8c92599fcc76eb93
user <user@Keevah.local>
2025-02-14 13:10:49 +0000

    connect 4

--- src/components/Connect4/Board.tsx
+++ src/components/Connect4/Board.tsx
@@ -7,7 +7,7 @@ const Board: React.FC = () => {
   const { board, gameOver } = useGameContext();
 
   return (
-    <div className="grid border-4 rounded-[10px] mb-6 grid-cols-7 gap-1">
+    <div className="grid border-4 w-full rounded-[10px] mb-6 grid-cols-7 gap-2">
       {board.map((row, rowIndex) =>
         row.map((ch, colIndex) => (
           <Slot key={`${rowIndex}-${colIndex}`} ch={ch} x={colIndex} y={rowIndex} />
