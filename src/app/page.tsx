c8758af5ea9b7f18de57e6fbcd30f9351d4169e6
user <keevahhphp@gmail.com>
2025-02-15 21:29:40 +0000

    connect 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Connect4 from "../components/home/connect";
 import Hero from "../components/home/home-hero";
 import Tictactoe from "../components/home/tic-tac-toe";
 
-
 export default function Home() {
   return (
     <div className="min-h-screen">
